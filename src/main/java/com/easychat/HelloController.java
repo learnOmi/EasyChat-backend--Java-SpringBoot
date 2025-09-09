1e4538738cf13ab9f720d75c0dad3888155f05d7
Tong <1427275295@qq.com>
2025-09-10 04:14:39 +0800

    initialize a backend springboot project

---
+++ src/main/java/com/easychat/HelloController.java
@@ -0,0 +1,13 @@
+package com.easychat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 表示这是一个REST控制器
+public class HelloController {
+
+    @GetMapping("/hello") // 处理GET请求到/hello路径
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+}
